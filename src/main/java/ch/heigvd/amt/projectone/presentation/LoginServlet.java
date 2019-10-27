20f842e12d8398a1014113e8d3ba20adce9a9d4d
OKI <olivier.koffi@heig-vd.ch>
2019-10-27 22:39:02 +0100

    login + dashboard + auth in progress, schema UML done

--- src/main/java/ch/heigvd/amt/projectone/presentation/LoginServlet.java
+++ src/main/java/ch/heigvd/amt/projectone/presentation/LoginServlet.java
@@ -1,13 +1,22 @@
 package ch.heigvd.amt.projectone.presentation;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Servlet in charge of the login
  */
 public class LoginServlet extends javax.servlet.http.HttpServlet {
+
+    private final String username = "admin";
+    private final String password = "adminpw";
 
     @Override
     public void init(ServletConfig config) throws ServletException {
@@ -16,6 +25,36 @@ public class LoginServlet extends javax.servlet.http.HttpServlet {
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
             throws javax.servlet.ServletException, IOException{
-        request.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
+        request.getRequestDispatcher("./WEB-INF/pages/login.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Just an example before the DAO implementation
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (this.username.equals(username) && this.password.equals(password)) {
+            //get the old session and invalidate
+            HttpSession oldSession = request.getSession(false);
+            if (oldSession != null) {
+                oldSession.invalidate();
+            }
+            //generate a new session
+            HttpSession newSession = request.getSession(true);
+
+            //setting session to expiry in 5 mins
+            newSession.setMaxInactiveInterval(5*60);
+
+            Cookie message = new Cookie("message", "Welcome");
+            response.addCookie(message);
+            request.getRequestDispatcher("./WEB-INF/pages/dashboard.jsp").forward(request, response);
+        } else {
+
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/loginPage.html");
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>Either username or password is wrong.</font>");
+            rd.include(request, response);
+        }
     }
 }
