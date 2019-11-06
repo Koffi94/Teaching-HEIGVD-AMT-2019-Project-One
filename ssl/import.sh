6bfadaa43f35db59db8b7fc25f4bf82fc105011f
NatMiz <nathanael.mizutani@heig-vd.ch>
2019-11-06 14:31:20 +0100

    Adde payara self-signed certificate and script (not tested) to import it

---
+++ ssl/import.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo keytool -cacerts -keystore  /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security/cacerts -storepass changeit -alias payara -import file ~/Documents/HEIG-VD/Cours_semestre5_2019/AMT/projects/Teaching-HEIGVD-AMT-2019-Project-One/ssl/payara-self-signed-certificate.crt
