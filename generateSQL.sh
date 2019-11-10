704efd54b4a50eee8c47b8994f555f821137014c
NatMiz <nathanael.mizutani@heig-vd.ch>
2019-11-10 19:35:55 +0100

    Script working and tested

--- generateSQL.sh
+++ generateSQL.sh
@@ -24,11 +24,11 @@ PROPERTY3="ATMOS"
 
 # Iterations
 typeset -i USERS_NUMBER=1000
-typeset -i MOVIES_NUMBER=55
-typeset -i CINEMAS_NUMBER=25
+typeset -i MOVIES_NUMBER=105
+typeset -i CINEMAS_NUMBER=55
 
 typeset -i MOVIES_PER_USER=50
-typeset -i CINEMAS_PER_USER=20
+typeset -i CINEMAS_PER_USER=25
 
 FILE=Docker-Images/DB-image/data/testDB.sql
 
@@ -47,30 +47,39 @@ echo "SET AUTOCOMMIT=0;" >> $FILE
 echo "INSERT INTO user VALUES" >> $FILE
 
 for ((u = 0; u < $USERS_NUMBER; u++));
-do echo "(DEFAULT, $USERNAME$u, $PASSWD$u)," >> $FILE
+do echo "(DEFAULT, '$USERNAME$u', '$PASSWD$u')," >> $FILE
 done
-echo "(DEFAULT, $USERNAME$USERS_NUMBER, $PASSWD$USERS_NUMBER);" >> $FILE
+echo "(DEFAULT, '$USERNAME$USERS_NUMBER', '$PASSWD$USERS_NUMBER');" >> $FILE
 echo "COMMIT;" >> $FILE
+
+echo "SELECT '<-------- Users inserted -------->' AS '';" >> $FILE
+echo "Users generated"
 
 # Generating movies
 echo "SET AUTOCOMMIT=0;" >> $FILE
 echo "INSERT INTO movie VALUES" >> $FILE
 
 for ((m = 0; m < $MOVIES_NUMBER; m++));
-do echo "(DEFAULT, $TITLE$m, $CATEGORY)," >> $FILE
+do echo "(DEFAULT, '$TITLE$m', '$YEAR', '$CATEGORY')," >> $FILE
 done
-echo "(DEFAULT, $TITLE$MOVIES_NUMBER, $CATEGORY);" >> $FILE  #TODO generate several category
+echo "(DEFAULT, '$TITLE$MOVIES_NUMBER', '$YEAR', '$CATEGORY');" >> $FILE
 echo "COMMIT;" >> $FILE
+
+echo "SELECT '<-------- Movies inserted -------->' AS '';" >> $FILE
+echo "Movies generated"
 
 # Generating cinemas
 echo "SET AUTOCOMMIT=0;" >> $FILE
 echo "INSERT INTO cinema VALUES" >> $FILE
 
 for ((c = 0; c < $CINEMAS_NUMBER; c++));
-do echo "(DEFAULT, $NAME$c, PRICE)," >> $FILE
+do echo "(DEFAULT, '$NAME$c', '$CITY$c', '$PRICE')," >> $FILE
 done
-echo "(DEFAULT, $NAME$CINEMAS_NUMBER, PRICE);" >> $FILE
+echo "(DEFAULT, '$NAME$CINEMAS_NUMBER', '$CITY$CINEMAS_NUMBER', '$PRICE');" >> $FILE
 echo "COMMIT;" >> $FILE
+
+echo "SELECT '<-------- Cinemas inserted -------->' AS '';" >> $FILE
+echo "Cinemas generated"
 
 # Generating screenings
 echo "SET AUTOCOMMIT=0;" >> $FILE
@@ -82,10 +91,12 @@ do
 	for ((mov = 1; mov < $MOVIES_PER_USER; mov++));
 	do 
 		for ((ci = 1; ci < $CINEMAS_PER_USER; ci++));
-		do echo "($mov:$ci, $ROOM1$mov, $PROPERTY1, $usr, $ci, $mov)," >> $FILE
+		do echo "(DEFAULT, '$mov:$ci', '$ROOM1$mov', '$PROPERTY1', $usr, $ci, $mov)," >> $FILE
 		done
 	done
 done
+
+echo "First half of users generated"
 
 # Second half of users
 for ((usr = 2; usr < $USERS_NUMBER; usr += 2));
@@ -93,13 +104,16 @@ do
 	for ((mov = 1; mov < $MOVIES_PER_USER; mov++));
 	do 
 		for ((cin = 1; cin < $CINEMAS_PER_USER; cin++));
-		do echo "($mov:$cin, $ROOM2$mov, $PROPERTY2, $usr, $cin, $mov)," >> $FILE
+		do echo "(DEFAULT, '$mov:$cin', '$ROOM2$mov', '$PROPERTY2', $usr, $cin, $mov)," >> $FILE
 		done
 	done
 done
 
-echo "(25:61, ${ROOM3}1, $PROPERTY3, $USERS_NUMBERS, $MOVIES_NUMBER, $CINEMAS_NUMBER);" >> $FILE
+echo "(DEFAULT, '25:61', '${ROOM3}1', '$PROPERTY3', $USERS_NUMBER, $MOVIES_NUMBER, $CINEMAS_NUMBER);" >> $FILE
 echo "COMMIT;" >> $FILE
+
+echo "SELECT '<-------- All data inserted -------->' AS '';" >> $FILE
+echo "All data generated"
 
 echo "SET SQL_MODE=@OLD_SQL_MODE;" >> $FILE
 echo "SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;" >> $FILE
